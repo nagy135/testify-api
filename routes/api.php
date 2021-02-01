b11ee6ae1e957f4b4cc138a9fd529d910b796462
Viktor Nagy <viktor.nagy1995@gmail.com>
2021-02-01 21:00:38 +0100

    fix: laravel 8 api route namespace

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/login', 'UserController@login');
+Route::post('/login', [UserController::class, 'login']);
