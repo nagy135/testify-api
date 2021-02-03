5d66fb5831a4b29367835c4df7a522dc1d84f697
Vladimir Osvald <vladimir.osvald@01people.com>
2021-02-03 16:05:13 +0100

    feat: backpack + passport install

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,16 @@
 
 namespace App\Models;
 
+use Backpack\CRUD\app\Models\Traits\CrudTrait;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasRoles, CrudTrait, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
--- database/migrations/2021_01_23_160834_create_users_table.php
+++ database/migrations/2021_01_23_160834_create_users_table.php
@@ -15,11 +15,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
             $table->string('email')->unique();
             $table->string('password');
-            $table->boolean('teacher');
+            $table->boolean('teacher')->nullable();
             $table->rememberToken();
 
             $table->unsignedBigInteger('class_id')->nullable();
