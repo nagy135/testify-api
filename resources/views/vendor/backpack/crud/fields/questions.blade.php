88d1178b40456e7453b26e908fdd4220db097281
Viktor Nagy <viktor.nagy1995@gmail.com>
2021-02-06 18:38:42 +0100

    feat: begin parsing inputs with data, type branching

--- resources/views/vendor/backpack/crud/fields/questions.blade.php
+++ resources/views/vendor/backpack/crud/fields/questions.blade.php
@@ -41,6 +41,68 @@
 
         <script>
             $(document).ready(function () {
+                // holds objects of data field later stored as models
+                let questions = [];
+
+                // creates another question object for later saving
+                function createQuestionHandler(type){
+                    let newQuestion = {
+                        type: type
+                    };
+                    let inputs = $("." + type).find('input');
+
+                    // here branch to different parsing methods depending on question type
+                    switch (type) {
+                        case 'multi-choice':
+                            questions.push(newMultiChoiceQuestion(newQuestion, inputs));
+                            break;
+                        case 'drag-join':
+                            questions.push(newDragJoinQuestion(newQuestion, inputs));
+                            break;
+                        case 'image':
+                            questions.push(newImageQuestion(newQuestion, inputs));
+                            break;
+                        case 'question-answer':
+                            questions.push(newQuestionAnswerQuestion(newQuestion, inputs));
+                            break;
+                        case 'yes-no':
+                            questions.push(newYesNoQuestion(newQuestion, inputs));
+                            break;
+                    };
+                    console.log('updated questions', questions);
+                };
+
+                function newMultiChoiceQuestion(newQuestion, inputs){
+                    inputs.each(function(){
+                        newQuestion[$(this).attr('name')] = $(this).val();
+                    });
+                    return newQuestion;
+                };
+                function newDragJoinQuestion(newQuestion, inputs){
+                    inputs.each(function(){
+                        newQuestion[$(this).attr('name')] = $(this).val();
+                    });
+                    return newQuestion;
+                };
+                function newImageQuestion(newQuestion, inputs){
+                    inputs.each(function(){
+                        newQuestion[$(this).attr('name')] = $(this).val();
+                    });
+                    return newQuestion;
+                };
+                function newQuestionAnswerQuestion(newQuestion, inputs){
+                    inputs.each(function(){
+                        newQuestion[$(this).attr('name')] = $(this).val();
+                    });
+                    return newQuestion;
+                };
+                function newYesNoQuestion(newQuestion, inputs){
+                    inputs.each(function(){
+                        newQuestion[$(this).attr('name')] = $(this).val();
+                    });
+                    return newQuestion;
+                };
+
                 let questionTypeMapping = [
                     "multi-choice",
                     "drag-join",
@@ -56,15 +118,19 @@
                     "Yes/No",
                 ];
 
+                // add new question with selected type
                 $("#add-question").click(function() {
                     let type = $("#question-types").val();
                     $('.question-type-creators').children().hide();
                     $('.' + questionTypeMapping[type]).show();
+                    $('#add-question-modal').data('question-type', questionTypeMapping[type]);
                     $('#add-question-modal h5.modal-title').html(questionTitleMapping[type]);
                 });
 
+                // this is case where question is completed and should be stored
                 $("#add-question-modal button.add-question-save").click(function(){
                     $('#add-question-modal').modal('hide');
+                    createQuestionHandler($('#add-question-modal').data('question-type'));
                 });
 
                 // image {
